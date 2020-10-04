656f92794f4bafa1ac8e31ff3cce779aedc4f585
Viktor Eikman <viktor.eikman@gmail.com>
2020-10-04 18:22:30 +0200

    Started work proper.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject dmote-drawer "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://FIXME.com/dmote-drawer"
+  :description "Printable assortment drawers for keyboards"
+  :url "https://viktor.eikman.se/article/the-dmote/"
   :license {:name "EPL-2.0 OR GPL-3.0-or-later"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.0"]
@@ -8,6 +8,7 @@
                  [environ "1.1.0"]
                  [scad-app "0.3.0"]
                  [scad-clj "0.5.3"]
-                 [scad-tarmi "0.6.0"]]
+                 [scad-tarmi "0.6.0"]
+                 [scad-klupe "0.2.0"]]
   :plugins [[lein-environ "1.1.0"]]
   :main ^:skip-aot dmote-drawer.core)
--- src/dmote_drawer/models.clj
+++ src/dmote_drawer/models.clj
@@ -2,18 +2,56 @@
 
 (ns dmote-drawer.models
   (:require [scad-clj.model :as model]
-            [scad-tarmi.core :refer [π]]))
+            [scad-tarmi.core :refer [π]]
+            [scad-tarmi.dfm :refer [error-fn]]
+            [scad-klupe.iso :refer [bolt]]))
 
 (def port-size [162 92 25])
 (def wall-thickness 2)
 (def dfm-margins [0.5 0.25 0.15])
+(def magnet-inset 4)  ; From the back of the port.
+(def r-rear 15)
+(def r-lesser 3)
+(def drawer-size (mapv - port-size dfm-margins))
+(def screw-position [(- (/ (first drawer-size) 2) magnet-inset)
+                     (- (second drawer-size) magnet-inset)])
+
+(let [[x y _] drawer-size
+      depth (- y r-lesser)]
+  (def outer-contour
+    (model/union
+      (model/translate [0 depth]
+        (model/hull
+          (model/translate [(+ (/ x -2) r-lesser) 0] (model/circle r-lesser))
+          (model/translate [(- (/ x 2) r-lesser) 0] (model/circle r-lesser))))
+      (model/translate [0 (/ depth 2)] (model/square x depth)))))
+
+(let [[x y _] (mapv - drawer-size (repeat 3 (* 2 wall-thickness)))]
+  (def inner-contour
+    (model/translate [0 wall-thickness]
+      (model/hull
+        (model/translate [(+ (/ x -2) r-rear) (- y r-rear)] (model/circle r-rear))
+        (model/translate [(- (/ x 2) r-rear) (- y r-rear)] (model/circle r-rear))
+        (model/translate [(+ (/ x -2) r-lesser) r-lesser] (model/circle r-lesser))
+        (model/translate [(- (/ x 2) r-lesser) r-lesser] (model/circle r-lesser))))))
+
+(def magnet-target
+  (model/translate [0 0 0.5]
+    (model/rotate [π 0 0]
+      (bolt {:m-diameter 3
+             :head-type :countersunk
+             :total-length (nth port-size 2)
+             :channel-length 2
+             :compensator (error-fn 0.5)}))))
 
 (defn base-model
   "Describe the geometry for one drawer."
   [options]
   (model/difference
-    (apply model/cube (mapv - port-size dfm-margins))
+    (model/extrude-linear {:height (nth drawer-size 2), :center false}
+                          outer-contour)
     (model/translate [0 0 wall-thickness]
-      (apply model/cube (mapv - port-size
-                                (repeat 3 (* 2 wall-thickness))
-                                dfm-margins)))))
+      (model/extrude-linear {:height (nth drawer-size 2), :center false}
+                            inner-contour))
+    (model/translate screw-position magnet-target)
+    (model/translate (update screw-position 0 -) magnet-target)))
